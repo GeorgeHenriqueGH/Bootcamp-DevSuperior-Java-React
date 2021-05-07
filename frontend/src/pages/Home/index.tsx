68a6aa988747f61217771707bff55d53219f2015
George <georgehenriquecampos@gmail.com>
2021-05-07 16:33:16 -0300

    Home, navigation

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (<>
+        <div className="container">
+            <div className="jumbotron bg-dark center">
+                <h1 className="display-4 text-primary">DSVendas</h1>
+                <p className="lead text-white">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                <hr />
+                <p className="text-white">Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+            </div>
+        </div>
+    </>);
+}
+
+export default Home;
